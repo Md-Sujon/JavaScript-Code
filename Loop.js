86c87e672a64a59454358458123cff6132fb4a68
Md-Sujon <mdsujon829562@gmail.com>
2021-06-27 00:36:48 +0600

    loop

---
+++ Loop.js
@@ -0,0 +1,14 @@
+// var num = 10;
+// while(num-->0){
+//     console.log(num);
+// }
+
+var i = 0;
+while(i <= 100){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+    i++;
+}
+    
+
---
+++ ternary.js
@@ -0,0 +1,15 @@
+var num = 7;
+var result 
+
+var reminder = num % 2;
+result = (0 == reminder) ? "Even" : "Odd" //if (?) else (:)
+console.log("This is ",result ,"Number");
+
+var number = -4;
+var result1 
+result1 = (0 <= number ) ? "positive" : "negative" //if (?) else (:)
+console.log("This is ", result1 ,"Number");
+
+
+
+
