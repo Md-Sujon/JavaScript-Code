13c94ac55c0aef23063d112f5bb2fa333642fff9
Md-Sujon <mdsujon829562@gmail.com>
2021-06-27 14:49:25 +0600

    series

--- Loop.js
+++ Loop.js
@@ -3,12 +3,26 @@
 //     console.log(num);
 // }
 
-var i = 0;
-while(i <= 100){
-    if(i % 2 == 0){
-        console.log(i);
-    }
-    i++;
-}
+// var i = 0;
+// while(i <= 5){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
     
+var j = 0;
+while(j < 10){
+    j+=2;
+    console.log(j);
+}
 
+var k = 0;
+while(true){
+    console.log(k);
+    if(30 == k){
+       break;
+    }
+    k+=1;
+
+}
---
+++ increment.js
@@ -0,0 +1,10 @@
+i = 0; 
+while (i++ < 10){
+    console.log(i);
+}
+
+console.log("\n reset \n");
+i = 0; 
+while (++i < 10){
+    console.log(i);
+}
