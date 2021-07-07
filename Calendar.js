0ee5e15de17216290f4b08c7523a7e5f7cf5ab51
Md-Sujon <mdsujon829562@gmail.com>
2021-07-07 15:54:22 +0600

    Calender

---
+++ Calendar.js
@@ -0,0 +1,54 @@
+var MonthName = "April"
+var StartingDay = 4;
+var Day = 28;
+console.log("Calendar Of Month",MonthName);
+console.log("Sun  Mon  Tue  Wed  Thu  Fri  Sat");
+
+
+for (var i = 0; i <5; i++) {
+    var DayRow = "";
+    for (var j = 1; j <=7; j++) {
+        var currentDay = 7*i + j-StartingDay;
+        if(currentDay > Day){
+            break;
+        };
+
+        if(currentDay<1){
+       currentDay =" "
+        }
+         if(currentDay>9){
+            DayRow += currentDay + "   ";
+         }else{
+            DayRow += currentDay + "    ";
+         }
+
+
+        
+        
+    }
+    console.log(DayRow)
+}
+
+
+
+// for(var i = 0; i < 5; i++) {
+//     var DayRow= "";
+//     for(var j = 1; j <= 7;j++){
+//         var CurrentDay = 7*i+j-StartingDay;
+//       if(CurrentDay > Day){
+//           break;
+//       };
+
+// if(CurrentDay<1){
+//     CurrentDay =" ";
+// }
+
+//       if(CurrentDay>9){
+//         DayRow += CurrentDay+"   ";
+//       }else{
+//         DayRow += CurrentDay+"    ";
+//       }
+        
+
+//     }console.log(DayRow);
+// }
---
+++ StringCase.js
@@ -0,0 +1,5 @@
+var promise = "SUJON";
+console.log(promise.toLowerCase());
+console.log(promise.toUpperCase());
+console.log(promise.indexOf("N"));
+console.log(promise.split("N"));
